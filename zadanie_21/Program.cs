77fb5b1eabc3923105df8658d33d7452bfb40a24
CreativeOZ <zagriaoleg@gmail.com>
2022-02-22 18:50:15 +0300

    палиндром.

---
+++ zadanie_21/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+Console.Clear();
+
+Console.WriteLine("Введите 5ти значное число: ");
+string num = Console.ReadLine();
+int number = Convert.ToInt32(num);
+if (number % 10 == number / 1000 && (number % 100) % 10 == (number/1000)%10)
+{
+    Console.WriteLine("Это палиндром");
+
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
+
+
+
